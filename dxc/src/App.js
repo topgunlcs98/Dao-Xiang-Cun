b2c326496622e4b02dd7efb18ae7dfb534bd36d9
topgunlcs98 <liuchangshu98@163.com>
2018-07-21 19:48:04 +0800

    test antd_Icon

--- dxc/src/App.js
+++ dxc/src/App.js
@@ -4,7 +4,7 @@ import {Icon} from 'antd'
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}> <Icon type="chrome" style={{ fontSize: 32, color: '#08c' }} />稻香村-front_end</div>
+      <div className={styles.App}> <Icon type="chrome" style={{ fontSize: 32, color: 'green' }} />稻香村-front_end<Icon type="github"style={{ fontSize: 32, color: 'black' }}  /></div>
     );
   }
 }
