3dadf22a893f31c3e6ac5508f344cdf557a3077a
ChrisCN97 <874147338@qq.com>
2018-07-30 19:32:32 +0800

    增加了推荐信息条 by CN

--- dxc/src/views/recommand/Recommand.jsx
+++ dxc/src/views/recommand/Recommand.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import styles from './recommand.css'
 import {Header,Footer}  from 'components/common'
+import Selection from './selection/selection'
+import ItemList from './item-list/itemList'
 
 export default class Recommand extends Component {
     render() {
@@ -9,8 +11,11 @@ export default class Recommand extends Component {
                 <div className={styles.headerWrapper}>
                     <Header />
                 </div>
-                <div className={styles.containtWrapper}>
-                      推荐内容
+                <div className={styles.selectionWrapper}>
+                    <Selection />
+                </div>
+                <div className={styles.itemListWrapper}>
+                    <ItemList />
                 </div>
                 <div className={styles.footerWrapper}>
                     <Footer />
---
+++ dxc/src/views/recommand/selection/selection.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import styles from './selection.css'
+
+export default class Selection extends Component {
+    render() {
+        return(
+            <div>
+                选择栏
+            </div>
+        )
+    }
+}
