bc19b5069568bb1da33cafbe223a55be407ccd39
Zhou Chen <756349535@qq.com>
2018-08-01 16:55:17 +0800

    new

--- dxc/src/views/home/components/product-recommand/ProductRecommand.jsx
+++ dxc/src/views/home/components/product-recommand/ProductRecommand.jsx
@@ -7,7 +7,7 @@ export default class ProductRecommand extends Component {
     constructor(props) {
         super(props);
         this.state={
-            content: [{id:0}, {id:1}, {id:2}, {id:3}, {id:4}, {id:5},{id:6}]
+            content: [{id:0}, {id:1}, {id:2}, {id:3}, {id:4}, {id:5}]
         }
     }
     render () {
