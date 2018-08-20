ebad181381169c867d4f20d84a891427e0541cdc
Zhou Chen <756349535@qq.com>
2018-08-20 17:34:50 +0800

    Projectdetails

--- dxc/src/views/project-detail/ProjectDetail.jsx
+++ dxc/src/views/project-detail/ProjectDetail.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import styles from './ProjectDetail.css'
 import {Header,Footer}  from 'components/common'
+import BriefIntro from './components/brief-intro/BriefIntro';
 
 export default class ProjectDetail extends Component {
     render() {
@@ -10,8 +11,7 @@ export default class ProjectDetail extends Component {
                     <Header />
                 </div>
                 <div className={styles.containtWrapper}>
-                    项目详情
-                    <div>简介</div>
+                    <BriefIntro />
                     <div>轮播图</div>
                     <div>信息</div>
                     <div>用户点评</div>
