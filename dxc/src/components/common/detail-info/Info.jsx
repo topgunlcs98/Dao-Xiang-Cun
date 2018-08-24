c46ca29ad0d61f1ea8a9eb7f79705368b48157ad
topgunlcs98 <liuchangshu98@163.com>
2018-08-24 21:42:04 +0800

    feat(Info) 按钮展开收起功能

--- dxc/src/components/common/detail-info/Info.jsx
+++ dxc/src/components/common/detail-info/Info.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
 import styles from './info.css'
+import { Button } from 'antd'
 
 export default class Info extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            title: props.type
+            title: props.type,
+            isFold: true
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick = () => {
+        this.setState({isFold:!this.state.isFold})
     }
     render() {
         return(
             <div className={styles.wrapper}>
                 <div className={styles.title}>{this.state.title}</div>
-                <div className={styles.content}>
+                <div className={styles.content}
+                    style={{height: this.state.isFold?80: 'auto'}}>
                     lalaallalala<br/>
-                    hahaha
+                    hahaha<br/>
+                    llll<br/>
+                    kkk<br/>
                 </div>
-                <div className={styles.more}>更多</div>
+                <div className={styles.more}> <Button onClick={this.handleClick}>{this.state.isFold?'更多': '收起'}</Button></div>
             </div>
         )
     }
