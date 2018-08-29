967b0288e5acb654c691f75eb8d51cf7d8caab36
topgunlcs98 <liuchangshu98@163.com>
2018-08-29 16:20:02 +0800

    feat(diary)顶部导航栏

---
+++ dxc/src/views/diary/components/diary/Diary.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export default class Diary extends Component {
+    render(){
+        return(
+            <div>发现好游记</div>
+        )
+    }
+}
---
+++ dxc/src/views/diary/components/write/Write.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react' 
+
+export default class Write extends Component {
+    render(){
+        return(
+            <div>我要写游记</div>
+        )
+    }
+}
